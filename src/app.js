31953ce0b9f891e7e34e5ce6c1926ea6f6589e4c
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-08 13:15:05 -0300

    feat: agregue archivos para las notas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "dotenv/config"; // carga las variables de entorno de .env al objeto proc
 import express from "express"; // framework que permite crear un servidor y definir las rutas
 import morgan from "morgan";
 import userRouter from "./routes/user.routes.js"; // importa el archivo de rutas del usuario
+import notesRouter from "./routes/notes.routes.js";
 
 const app = express(); // crea una instancia de express para poder acceder a ella
 
@@ -11,6 +12,7 @@ app.use(morgan("dev")); //morgan sirve para registrar las peticiones y mostrarla
 
 // RUTAS
 app.use("/api/users", userRouter); // ruta de usuario
+app.use("/api/notes", notesRouter);
 
 // SERVIDOR
 const PORT = process.env.PORT; // obtiene el puerto de la variable de entorno PORT
