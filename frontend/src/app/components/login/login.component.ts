78b07ae8c75046d285609c55facffe5d9c32c0fc
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-10 15:17:34 -0300

    feat: agregue formulario reactivo para el inicio de sesion

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIcon } from '@angular/material/icon';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -16,8 +23,26 @@ import { MatIcon } from '@angular/material/icon';
     MatButtonModule,
     MatCardModule,
     MatIcon,
+    ReactiveFormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  // form = new FormGroup({ // forma de crear un formulario antigua
+  //   username: new FormControl(''),
+  //   password: new FormControl(''),
+  // });
+
+  private readonly _formBuilder = inject(FormBuilder);
+
+  form = this._formBuilder.group({
+    username: '',
+    password: '',
+  });
+
+  clickLogin(): void {
+    const username = this.form.controls.username.value;
+    console.log(username);
+  }
+}
