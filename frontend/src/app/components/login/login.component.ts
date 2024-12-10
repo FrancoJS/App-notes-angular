ef2de6263b5a50eeb0e7e1e913d914596e259be1
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-10 00:04:32 -0300

    feat: agregue componente de login con angular material, falta conectarlo con la API

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIcon,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
