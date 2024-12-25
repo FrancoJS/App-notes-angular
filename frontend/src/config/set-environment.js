76b6c0814caa493be0a2e8e499ecb86a54857fae
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-25 16:19:51 -0300

    pruebas

--- frontend/src/config/set-environment.js
+++ frontend/src/config/set-environment.js
@@ -4,13 +4,17 @@ const path = require('path');
 const authUrl = process.env.AUTH_URL;
 const notesUrl = process.env.NOTES_URL;
 
+const environmentDirPath = path.join(__dirname, '../environments');
 const environmentPathDevelopment = path.join(__dirname, '../environments/environment.development.ts');
 const environmentPath = path.join(__dirname, '../environments/environment.ts');
 
-// if (!fs.existsSync(environmentPath)) {
-//   fs.mkdirSync(environmentPath, { recursive: true });
-//   fs.mkdirSync(environmentPathDevelopment, { recursive: true });
-// }
+if (!fs.existsSync(environmentDirPath)) {
+  // Si no existe la carpeta, la creamos ya que railway elimina carpetas vacias
+  fs.mkdirSync(environmentDirPath, { recursive: true });
+}
+
+console.log(environmentPath);
+console.log(environmentPathDevelopment);
 
 const environmentContent = `
 export const environment = {
@@ -26,5 +30,5 @@ export const environment = {
   NOTES_URL: '${notesUrl}',
 };`;
 
-fs.writeFileSync(environmentPath, environmentContent);
-fs.writeFileSync(environmentPathDevelopment, environmentDevelopmentContent);
+// fs.writeFileSync(environmentPath, environmentContent);
+// fs.writeFileSync(environmentPathDevelopment, environmentDevelopmentContent);
