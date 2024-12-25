ab96848e68369820099eb9f39bb492f62864f0a9
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-25 17:06:43 -0300

    fix: arregle la configuracion de los environments

--- frontend/server.js
+++ frontend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,10 +9,13 @@ app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(express.static(__dirname + '/dist/browser'));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
+  console.log(__dirname);
+  console.log(path.join(__dirname, '/dist/browser/index.html'));
   res.sendFile(__dirname + '/dist/browser/index.html');
 });
 
 app.listen(PORT, () => {
+  console.log(__dirname);
   console.log(`Server running on port ${PORT}`);
 });
