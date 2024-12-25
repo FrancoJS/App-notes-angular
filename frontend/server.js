d0acf73837e31b8ac9c588c01ec4aeb9e4b588d4
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-25 18:11:46 -0300

    pruebas

--- frontend/server.js
+++ frontend/server.js
@@ -4,13 +4,12 @@ const app = express();
 const cors = require('cors');
 const httpProxyMiddleware = require('http-proxy-middleware');
 
+const backend_url = process.env.BACKEND_URL;
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 app.use(express.static(__dirname + '/dist/frontend/browser'));
-
-const backend_url = process.env.BACKEND_URL;
 
 app.use(
   '',
@@ -19,7 +18,6 @@ app.use(
     changeOrigin: true,
   }),
 );
-console.log(backend_url);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/dist/frontend/browser/index.html');
