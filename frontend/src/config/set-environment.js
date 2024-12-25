b5190ff584a0eb26e459ba5e72b1b8b9b3ee7508
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-25 15:55:02 -0300

    fix: arregle problema en el cual intentaba reemplazar un archivo de environment al momento de hacer un ng serve en railway

--- frontend/src/config/set-environment.js
+++ frontend/src/config/set-environment.js
@@ -5,18 +5,22 @@ const { verRuta } = require('../environments/ruta');
 const authUrl = process.env.AUTH_URL;
 const notesUrl = process.env.NOTES_URL;
 
+const environmentPathDevelopment = path.join(__dirname, '../environments/environment.development.ts');
 const environmentPath = path.join(__dirname, '../environments/environment.ts');
-
-console.log(environmentPath);
-console.log(path.resolve(environmentPath));
-console.log(process.cwd());
-console.log(__dirname);
-verRuta();
 
 const environmentContent = `
 export const environment = {
+  production: true,
+  AUTH_URL: '${authUrl}',
+  NOTES_URL: '${notesUrl}',
+};`;
+
+const environmentDevelopmentContent = `
+export const environment = {
+  production: false,
   AUTH_URL: '${authUrl}',
   NOTES_URL: '${notesUrl}',
 };`;
 
 fs.writeFileSync(environmentPath, environmentContent);
+fs.writeFileSync(environmentPathDevelopment, environmentDevelopmentContent);
--- frontend/src/environments/ruta.js
+++
@@ -1,5 +0,0 @@
-const verRuta = () => {
-  console.log(__dirname);
-};
-
-module.exports = { verRuta };
