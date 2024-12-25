9ef1ff3c93fdc3a53ccb68a654588163680c71df
Franco_SJ <jean.jara.fs@gmail.com>
2024-12-25 15:08:43 -0300

    fix: agregue importaciones de set-environment para que no fueran de tipo modulo y ocuparan el require

--- frontend/config/set-environment.js
+++ frontend/config/set-environment.js
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+const fs = require('fs');
+const path = require('path');
 
 const authUrl = process.env.AUTH_URL;
 const notesUrl = process.env.NOTES_URL;
